cb557fb9f160b24d16c011007875ab917a79d11c
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2020-11-01 17:33:55 +0500

    WIP: Adding async_get

--- api_client.cpp
+++ api_client.cpp
@@ -126,6 +126,58 @@ struct async_connect_initiation2
   }
 };
 
+struct async_request
+{
+  beast::tcp_stream &                              stream_;
+  std::unique_ptr<http::request<http::empty_body>> req_;
+  enum
+  {
+    request_in_progress,
+    waiting_for_response,
+  } state_;
+  beast::flat_buffer                                 buffer_;
+  std::shared_ptr<http::response<http::string_body>> res_;
+
+  async_request(beast::tcp_stream &stream, std::unique_ptr<http::request<http::empty_body>> req)
+    : stream_(stream)
+    , req_(std::move(req))
+    , state_(request_in_progress)
+
+  {
+    res_ = std::make_shared<http::response<http::string_body>>();
+  }
+
+  template <typename Self>
+  void operator()(Self &self)
+  {
+    stream_.expires_after(std::chrono::seconds(30));
+    http::async_write(stream_, *req_, std::move(self));
+  }
+
+  template <typename Self>
+  void operator()(Self &self, const boost::system::error_code &error,
+                  const std::size_t bytes_transferred)
+  {
+    if (!error)
+    {
+      switch (state_)
+      {
+      case request_in_progress:
+        http::async_read(stream_, buffer_, *res_, std::move(self));
+        state_ = waiting_for_response;
+        break;
+      case waiting_for_response:
+        // std::cout << *res_ << std::endl;
+        self.complete(error);
+      }
+    }
+    else
+    {
+      self.complete(error);
+    }
+  }
+};
+
 struct api_client
 {
   boost::asio::io_context &io_;
@@ -134,13 +186,17 @@ struct api_client
   const std::string        base_url_;
   const std::string        port_;
 
-  api_client(boost::asio::io_context &io, const std::string &base_url)
+  api_client(boost::asio::io_context &io, const std::string &base_url,
+             const std::string &port = "80")
     : io_(io)
     , stream_(io)
     , resolver_(io)
     , base_url_(base_url)
-    , port_("80")
+    , port_(port)
   {}
+  ~api_client()
+  {}
+
   template <typename CompletionToken>
   auto async_resolve_and_connect(CompletionToken &&token) -> typename boost::asio::async_result<
       typename std::decay<CompletionToken>::type,
@@ -159,6 +215,23 @@ struct api_client
         },
         token);
   }
+
+  template <typename CompletionToken>
+  auto async_get(const std::string &path, CompletionToken &&token) ->
+      typename boost::asio::async_result<typename std::decay<CompletionToken>::type,
+                                         void(const boost::system::error_code &)>::return_type
+  {
+    std::unique_ptr<http::request<http::empty_body>> req =
+        std::make_unique<http::request<http::empty_body>>();
+
+    req->version(11);
+    req->method(http::verb::get);
+    req->target(path);
+    req->set(http::field::host, base_url_);
+
+    return boost::asio::async_compose<CompletionToken, void(const boost::system::error_code &)>(
+        async_request(stream_, std::move(req)), token);
+  }
 };
 //------------------------------------------------------------------------------
 
@@ -169,10 +242,13 @@ void test_callback()
   api_client client(io_context, "www.google.com");
 
   client.async_resolve_and_connect(
-      [&io_context](const boost::system::error_code &error, auto endpoint) {
+      [&client](const boost::system::error_code &error, auto endpoint) {
         if (!error)
         {
           std::cout << "connected at " << *endpoint << std::endl;
+          client.async_get("/", [](const boost::system::error_code &error) {
+            std::cout << "async_get: " << error.message() << "\n";
+          });
         }
         else
         {
@@ -198,16 +274,27 @@ void test_future()
     auto endpoint = c.get();
 
     std::cout << "connected at " << *endpoint << std::endl;
+
+    try
+    {
+      std::future req = client.async_get("/", boost::asio::use_future);
+      io_context.run();
+      req.get();
+    }
+    catch (const std::exception &e)
+    {
+      std::cout << "get error: " << e.what() << "\n";
+    }
   }
   catch (const std::exception &e)
   {
-    std::cout << "Error: " << e.what() << "\n";
+    std::cout << "connect error: " << e.what() << "\n";
   }
 }
 
 int main(void)
 {
-  test_callback();
+  // test_callback();
   test_future();
   return 0;
 }
